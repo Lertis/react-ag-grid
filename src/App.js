8f4f4ade3d5cc040a2524905ebcc41d73965d27e
Yura Husak <yura.husak@juriba.com>
2020-04-04 21:27:50 +0300

    Added ag-grid enterprise to allow features

--- src/App.js
+++ src/App.js
@@ -4,14 +4,25 @@ import { AgGridReact } from 'ag-grid-react';
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-balham.css';
 
+import 'ag-grid-enterprise';
+
 export default class App extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			columnDefs: [
-				{ headerName: "Make", field: "make" },
-				{ headerName: "Model", field: "model" },
-				{ headerName: "Price", field: "price" }],
+			columnDefs: [{
+				headerName: "Make", field: "make", rowGroup: true
+			}, {
+				headerName: "Price", field: "price"
+			}],
+			autoGroupColumnDef: {
+				headerName: "Model",
+				field: "model",
+				cellRenderer: 'agGroupCellRenderer',
+				cellRendererParams: {
+					checkbox: true
+				}
+			},
 			rowData: [
 				{ make: "Toyota", model: "Celica", price: 35000 },
 				{ make: "Ford", model: "Mondeo", price: 32000 },
@@ -21,9 +32,11 @@ export default class App extends React.Component {
 
 	render() {
 		return (
-			<div className="ag-theme-balham" style={{ height: '200px', width: '600px' }}>
+			<div className="ag-theme-balham" style={{ height: '250px', width: '600px' }}>
 				<AgGridReact
 					columnDefs={this.state.columnDefs}
+					groupSelectsChildren={true}
+					autoGroupColumnDef={this.state.autoGroupColumnDef}
 					rowData={this.state.rowData}>
 				</AgGridReact>
 			</div>
